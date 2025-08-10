195e8ce84728482df193c1c9ff731cfb7e4f61d5
hyun907 <seunghyun020907@naver.com>
2025-08-10 23:42:20 +0900

    feat: add react-native-maps

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import React from "react";
+import MapView from "react-native-maps";
+
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <MapView style={{ flex: 1 }} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    width: "100%",
+    height: "100%"
+  }
+});
