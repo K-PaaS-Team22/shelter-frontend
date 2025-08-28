95538fa803f74c459cc4e2097f1336ae617025f4
hyun907 <seunghyun020907@naver.com>
2025-08-29 03:24:21 +0900

    feat: 구글 지도 적용

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import MapView from "react-native-maps";
-
 import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <MapView style={{ flex: 1 }} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.5665, // 서울 좌표
+          longitude: 126.978,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+      />
     </View>
   );
 }
