79be61b2ac9179f3e97a823b5da397d5f918add2
hyun907 <seunghyun020907@naver.com>
2025-08-29 04:42:06 +0900

    feat: 구글 지도 세팅 및 사용자 현재 위치 렌더링

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import MapView from "react-native-maps";
 import { StyleSheet, View } from "react-native";
+import LocationTracker from "../shared/components/LocationTracker";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.5665, // 서울 좌표
-          longitude: 126.978,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-      />
+      <LocationTracker />
     </View>
   );
 }
@@ -23,9 +13,5 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  },
-  map: {
-    width: "100%",
-    height: "100%"
   }
 });
