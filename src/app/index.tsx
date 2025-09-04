b3b665dbdce317eaf8b13a8d872cdb530db63a24
ahcgnoej <jinjinchachajung@naver.com>
2025-09-04 18:27:52 +0900

    chore: styleSheet 추가

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import Login from "@moeum/features/user/login/Login";
-import SignUp from "@moeum/features/user/sigin-up/SiginUp";
+import { StyleSheet } from "react-native";
 
 export default function LoginScreen() {
   return <Login />;
